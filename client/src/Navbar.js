d427533134bae1f61c740f7f50f60a39cf46efb2
mairacagri <mairacagri@gmail.com>
2022-05-21 12:54:14 +0100

    This is my commit message

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,14 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const NavBar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="quiz">Quiz</Link>
-      <Link to="stories">Stories</Link>
-      <Link to="photos">Photos</Link>
-    </div>
+    // <div>
+    //   <Link to="/">Home</Link>
+    //   <Link to="quiz">Quiz</Link>
+    //   <Link to="stories">Stories</Link>
+    //   <Link to="photos">Photos</Link>
+    // </div>
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+       Quizz
+      </a>
+      <button className="hamburger">
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className="navigation-menu">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
