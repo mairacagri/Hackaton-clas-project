7115c16a4667d4d0bbc8ecd33a3e80c88f262458
MatildaAko <matildaako@gmail.com>
2022-05-21 14:31:57 +0100

    Set up quiz

--- server/src/createServer.js
+++ server/src/createServer.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = require('./routes');
-const cors = require('cors');
+const express = require("express");
+const router = require("./routes");
+const cors = require("cors");
 
-module.exports = function (){
+module.exports = function () {
   const app = express();
   app.use(express.json());
-  app.use(cors())
+  app.use(cors({ origin: "*" }));
   app.use("/", router);
-  return app
-}
+  return app;
+};
