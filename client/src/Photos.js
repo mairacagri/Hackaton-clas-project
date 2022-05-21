8d8e5e29827f1fc9ba4e63ab0dcc9c582e01fb80
hachi-ops <elen20@protonmail.com>
2022-05-21 14:16:11 +0100

    image-slider component

--- client/src/Photos.js
+++ client/src/Photos.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Photos = () => {};
+// import BackgroundSlider from "react-background-slider";
+
+const images = [
+  "./images/1.jpg",
+  "./images/2.jpg",
+  "./images/3.jpg",
+  "./images/4.jpg",
+  "./images/5.jpg",
+  "./images/6.jpg",
+  "./images/7.jpg",
+  "./images/8.jpg",
+  "./images/9.jpg",
+  "./images/10.jpg",
+  "./images/11.jpg",
+  "./images/12.jpg",
+  "./images/13.jpg",
+  "./images/14.jpg",
+  "./images/15.jpg",
+];
+
+const Photos = () => {
+  //   return <BackgroundSlider images={images} duration={10} transition={2} />;
+  const [count, setCount] = useState(0);
+
+  function changeImage() {
+    setCount(count + 1);
+    console.log("Hi");
+  }
+  return (
+    <div>
+      {/* {images.map((image) => {
+        return (
+          <div>
+            <img src={image} alt="" />
+          </div>
+        );
+      })} */}
+      <img src={images[count]} alt="" onClick={changeImage} />
+    </div>
+  );
+};
 
 export default Photos;
