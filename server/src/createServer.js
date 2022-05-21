ca6ada6381e42e62615b85e9ae924e7b9a7a72b8
Berkeli Halmyradov <berkelih@gmail.com>
2022-05-21 14:00:40 +0100

    quiz FE working

--- server/src/createServer.js
+++ server/src/createServer.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const router = require('./routes');
-const cors = require('cors');
+const express = require("express");
+const router = require("./routes");
+const cors = require("cors");
 
-module.exports = function (){
+module.exports = function () {
   const app = express();
   app.use(express.json());
-  app.use(cors())
+  app.use(
+    cors({
+      allowedHeaders: ["userId"],
+    })
+  );
   app.use("/", router);
-  return app
-}
+  return app;
+};
--- server/src/routes/resolvers/quiz.js
+++ server/src/routes/resolvers/quiz.js
@@ -1,42 +1,44 @@
-const quizUser = require('../../utils/quizUser');
-const _ = require('lodash');
+const quizUser = require("../../utils/quizUser");
+const _ = require("lodash");
+const questions = require("../../questions-data.json");
 
 const users = {};
 
 function getNextQuestion(req, res) {
-    let userId = req.headers.userId;
-    if (!userId) {
-        userId = _.uniqueId("user_");
-        users[userId] = new quizUser(userId)
-    }
-    res.set('userId', userId);
-    let question = users[userId].getNextQuestion();
-    res.json({
-        question: question.question,
-        answers: question.answers,
-        id: question.id
-    });
+  let userId = req.headers.userId;
+  if (!userId || !users[userId]) {
+    userId = _.uniqueId("user_");
+    users[userId] = new quizUser(userId);
+  }
+  let question = users[userId].getNextQuestion();
+  res.json({
+    userId,
+    question: question.question,
+    answers: question.answers,
+    id: question.id,
+  });
 }
 
 function checkAnswer(req, res) {
-    let userId = req.headers.userId;
-    let questionId = req.params.id;
-    let answerId = req.params.answer;
-    users[userId].setAnswer(questionId, answerId);
-    if (users[userId].questions.length === 0) {
-        res.json({
-            score: users[userId].getScore()
-        });
-    }
-    const correctAnswer = questions.find(({id}) => id === questionId).correct
+  let userId = req.query.userId;
+  console.log(userId)
+  let questionId = req.params.id;
+  let answerId = req.params.answer;
+  users[userId].setAnswer(questionId, answerId);
+  if (users[userId].questions.length === 0) {
     res.json({
-        userAnswer: answerId,
-        correctAnswer,
-        correct: correctAnswer === answerId
+      score: users[userId].getScore(),
     });
+  }
+  const correctAnswer = questions.find(({ id }) => id == questionId).correct;
+  res.json({
+    userAnswer: answerId,
+    correctAnswer,
+    correct: correctAnswer == answerId,
+  });
 }
 
 module.exports = {
-    getNextQuestion,
-    checkAnswer
-}
+  getNextQuestion,
+  checkAnswer,
+};
